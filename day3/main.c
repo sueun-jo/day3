52b31df02815703677fab5d3983b9b9c8375c65c
sueun <absorbme@naver.com>
2025-04-25 09:31:17 +0900

    초기 커밋.

---
+++ day3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int total = 0;
+	int num = 0;
+
+	do
+	{
+		printf("정수 입력 (0 to quit): ");
+		scanf_s("%d", &num); //더할 num입력 받음
+
+		total += num; //total에 입력받은 숫자 더해준다
+
+	} while (num != 0); //num이 0이 아니면 계속 반복, 0이면 반복종료
+
+	printf("total: %d", total); //total값 출력
+
+	return 0;
+}
